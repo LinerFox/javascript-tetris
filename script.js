fd8a884cd5bd00acaf8f15db4e11719e40b74de3
NezumiCode <karkinoscode@gmail.com>
2022-08-07 18:10:11 -0400

    Added lTetromino layout using arrays

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const grid = document.querySelector(".grid");
-    let squares = Array.from(document.querySelector(".grid div"));
+    let squares = Array.from(document.querySelectorAll(".grid div"));
     const width = 10;
     console.log(squares);
+
+    // The Tetrominoes
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
+
+    const theTetrominoes=[lTetromino]
 });
+
