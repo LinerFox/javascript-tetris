efd1e2778d05b488c9137258c6263adb4ddb51a2
NezumiCode <karkinoscode@gmail.com>
2022-08-07 05:03:47 -0400

    Added starting JS Code

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.querySelector(".grid");
+    let squares = Array.from(document.querySelector(".grid div"));
+    const width = 10;
+    console.log(squares);
+});
