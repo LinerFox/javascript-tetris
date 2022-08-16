e0f75715c2b39e2f257af32496984fed7f5122b5
NezumiCode <karkinoscode@gmail.com>
2022-08-16 04:41:33 -0400

    Current Version for the moment

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const grid = document.querySelector(".grid");
-    let squares = Array.from(document.querySelector(".grid div"));
-    const width = 10;
-    console.log(squares);
+  const grid = document.querySelector(".grid");
+  let squares = Array.from(document.querySelector(".grid div"));
+  const width = 10;
+  console.log(squares);
 });
